c55953124f2e3c6ed608d9aba774e74c1f4a5d98
Jesús Paredes <jjesus.paredesl@gmail.com>
2023-07-26 20:28:47 -0300

    2/3 error en localhost

--- PreEntrega/src/daos/mongodb/connection.js
+++ PreEntrega/src/daos/mongodb/connection.js
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
 
 
-const connectionString = 'mongodb://localhost:27017/coderhouse'
+const connectionString = 'mongodb://localhost:27017/products'
 
 try{
     await mongoose.connect(connectionString);
     console.log('conectado a la base de datos MongoDB')
 } catch (error){
-    console.log(error)
+    console.log('Error al conectar a la base de datos:', error)
 }
--- PreEntrega/src/daos/mongodb/models/product.model.js
+++ PreEntrega/src/daos/mongodb/models/product.model.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 const productSchema = new mongoose.Schema({
-name: {type: String, required: true},
+title: {type: String, required: true},
 description : {type: String},
 price: {type: Number, required: true},
 stock: {type: Number, required: true}
